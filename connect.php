76ebca9de808648b88828d5e5e4e06dcb80b1074
Bui Tran Phuong Thao <121841993+thaobtpgcc210031@users.noreply.github.com>
2023-06-21 16:34:02 +0700

    thaoupdateconnect

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php 
-$conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'wzi8dspuerk2otep', 'x60opv7mpv18jt3e','wr2wm5pyhvoiu4mr')
+$conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'lfd252hh9kq36diq', 'k4ylciymtmaobo7h','z0srl15gkx5xttde')
 or die("Can not connect database".mysqli_connect_error());
 ?>
