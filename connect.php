c35a7a2070f7e017b1a5ac72fee860e84649f651
Bui Tran Phuong Thao <121841993+thaobtpgcc210031@users.noreply.github.com>
2023-06-22 23:07:28 +0700

    thaoupdatecon

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php 
-$conn = mysqli_connect('localhost', 'root', '','onlineshoppingn')
+$conn = mysqli_connect('u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'ge74p86a530pnod5', 'jq2ylwdc1xtngl7r','vpknz7css586xjq5')
 or die("Can not connect database".mysqli_connect_error());
 ?>
