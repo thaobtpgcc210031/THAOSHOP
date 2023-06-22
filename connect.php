317d8ecbf0bcd7f4a00d9d786f19907a87d8be1c
Bui Tran Phuong Thao <121841993+thaobtpgcc210031@users.noreply.github.com>
2023-06-22 23:06:46 +0700

    thaoupdateconnection

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php 
-$conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'lfd252hh9kq36diq', 'k4ylciymtmaobo7h','z0srl15gkx5xttde')
+$conn = mysqli_connect('localhost', 'root', '','onlineshoppingn')
 or die("Can not connect database".mysqli_connect_error());
 ?>
--- connect111.php
+++ connect111.php
@@ -1,4 +1,4 @@
 <?php 
-$conn = mysqli_connect('pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j1eqicbk3aymmhq3', 'tio4crr6ju6u5aze','k26n0dqtnq0jpkka')
+$conn = mysqli_connect('u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'ge74p86a530pnod5', 'jq2ylwdc1xtngl7r','vpknz7css586xjq5')
 or die("Can not connect database".mysqli_connect_error());
 ?>
